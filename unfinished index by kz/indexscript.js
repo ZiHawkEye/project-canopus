66fdc9606d83c43288ea0eeb7430d41346b07b73
ZiHawkEye <37863247+ZiHawkEye@users.noreply.github.com>
2018-04-02 20:24:00 +0800

    removed unfinished index

--- unfinished index by kz/indexscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	//submits for search bar when enter key is pressed
-    $('#search').keydown(function(event) {
-        if (event.keyCode == 13) {
-            this.form.submit();
-            return false;
-         }
-    });
-});
