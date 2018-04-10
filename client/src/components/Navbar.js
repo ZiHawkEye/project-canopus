9d05c3795032bcc1299f29efe0e1fea31ea48c79
ZiHawkEye <37863247+ZiHawkEye@users.noreply.github.com>
2018-04-10 17:40:58 +0800

    incorporated react framework; moved all previous work to obsolete folder

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../assets/Logo.svg';
+
+class Navbar extends React.Component {
+	
+	render() {
+		return (
+			<div>
+				<nav className="navbar">
+                <div className="logo">
+                    <img src={logo} alt="Logo"/>
+                </div>
+                    <div className="navlinks">
+                        <ul>
+                            <li><a href="#home" className="btn">Home</a></li>
+                            <li><a href="#notes" className="btn">Notes</a></li>
+                            <li><a href="#blog" className="btn">Blog</a></li>
+                            <li><a href="#about" className="btn">About</a></li>
+                        </ul>
+                    </div>
+                </nav>
+    		</div>
+		);
+	}
+}
+
+export default Navbar;
